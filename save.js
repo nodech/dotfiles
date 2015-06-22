c0d7ca6bc5d436afecff12196f6aad781aaff2f3
Node Chkuaselidze <nodar.chkuaselidze@gmail.com>
2015-06-22 11:49:48 -0400

    Add tmux stuff

--- save.js
+++ save.js
@@ -5,21 +5,71 @@ var path = require('path'),
     exec = require('child_process').exec,
     files = fs.readdirSync(__dirname),
     async = require('async'),
-    exclude = [ 'save.js', '.git', 'README.md', '.gitignore' ],
-    homeDir = path.resolve(process.env['HOME']);
+    util = require('util'),
+    exclude = [ 'save.js', '.git', 'README.md', '.gitignore', 'package.json', 'node_modules', 'save.sh', 'npm-debug.log' ],
+    homeDir = path.resolve(process.env['HOME']),
+    message = console.log.bind(null, '======= %s =======');
+
 
 files = files.filter(function (file) {
   return exclude.indexOf(file) < 0;
 });
 
-files.forEach(function (file) {
-  exec('cp -rv "' + file + '" "' + homeDir + '/"', function (error, stdout, stderr) {
-    console.log('cp -rv "' + file + '" "' + homeDir + '/"');
+async.series([
+  function (done) {
+    message('START COPYING');
+    async.each(files, function (file, callback) {
+      exec('cp -rv "' + file + '" "' + homeDir + '/"', function (error, stdout, stderr) {
+        console.log('cp -rv "' + file + '" "' + homeDir + '/"');
+        if (error) {
+          callback(error);
+          return
+        }
+
+        LOG(stdout, stderr);
+        callback();
+      });
+    }, done);
+  },
+  function (done) {
+    message('TMUX STUFF');
+    async.series([
+      installTmuxPackageManager,
+      installTmuxPackages
+    ], function (error) {
+      message('FINISHED TMUX STUFF');
+      done(error);
+    });
+  }
+], function (error, done) {
+});
+
+function installTmuxPackageManager(done) {
+  exec('git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm', function (error, stdout, stderr) {
     if (error) {
-      //throw error;
+      done(error);
+      return;
     }
 
-    console.log(stdout);
-    console.error(stderr);
+    LOG(stdout, stderr);
+    message('INSTALLED TMUX PACKAGE MANAGER');
+    done();
   });
-});
+}
+
+function installTmuxPackages(done) {
+  exec('~/.tmux/plugins/tpm/scripts/install_plugins.sh', function (error, stdout, stderr) {
+    if (error) {
+      done(error);
+      return;
+    }
+
+    LOG(stdout, stderr);
+    message('INSTALLED TMUX PACKAGES');
+  });
+};
+
+function LOG(out, err) {
+  console.log(out);
+  console.error(err);
+}
