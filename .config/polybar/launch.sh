555c95cf017bd1617a2b7ae6f11504c792797022
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2021-05-12 17:24:43 +0400

    update polybar cfgs.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+i=1
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar node &
+  echo polybar --config=$HOME/.config/polybar/config-$i node &
+  MONITOR=$m polybar --config=$HOME/.config/polybar/config-$i node &
+  i=$((i + 1))
 done
 
 echo "Bars launched..."
