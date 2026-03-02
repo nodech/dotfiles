948d4ea1215ac0fded808d665d898e23085cef95
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-03-02 19:28:30 +0400

    fix(git/commit-msg): move hyphen to the end.

--- nvim/lua/nod/lsp/bash_ls.lua
+++ nvim/lua/nod/lsp/bash_ls.lua
@@ -1,11 +1,5 @@
 ---@brief
----
---- https://github.com/bash-lsp/bash-language-server
----
---- `bash-language-server` can be installed via `npm`:
---- ```sh
---- npm i -g bash-language-server
---- ```
+--- `pacman -S bash-language-server`:
 ---
 --- Language server for bash, written using tree sitter in typescript.
 
