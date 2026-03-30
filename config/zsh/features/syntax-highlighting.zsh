b1ff578b1803c9a2a0a12505bc2f159e25aa5c1d
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-03-30 17:55:12 +0400

    feat(zsh): add syntax hl.

---
+++ config/zsh/features/syntax-highlighting.zsh
@@ -0,0 +1,17 @@
+# Load syntax highlighting late so it can wrap previously defined widgets.
+typeset -a _zsh_syntax_hl_candidates=(
+  /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+)
+
+for _zsh_syntax_hl_file in "${_zsh_syntax_hl_candidates[@]}"
+do
+  if [ -r "$_zsh_syntax_hl_file" ]; then
+    source "$_zsh_syntax_hl_file"
+    break
+  fi
+done
+
+unset _zsh_syntax_hl_candidates
+unset _zsh_syntax_hl_file
