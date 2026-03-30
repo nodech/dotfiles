ee1af3beb9f1409ef36d4e872cc6d2806202351c
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-03-30 22:57:36 +0400

    feat(zsh/git): add more aliases.

--- config/zsh/features/git.zsh
+++ config/zsh/features/git.zsh
@@ -12,6 +12,19 @@ alias gr="git remote"
 alias gco="git checkout"
 alias gc="git commit --verbose"
 alias gp="git push"
+alias gl="git pull"
+alias gsh="git show"
+alias gshps="git show --pretty=short --show-signature"
+
+# Logs
+alias glo="git log --oneline --decorate"
+alias glog="git log --oneline --decorate --graph"
+alias gloga="git log --oneline --decorate --graph --all"
+
+alias glg="git log --stat"
+alias glgp="git log --stat --patch"
+alias glgg="git log --graph"
+alias glgga="git log --graph --decorate --all"
 
 # Worktree
 alias gwt="git worktree"
@@ -19,3 +32,11 @@ alias gwta="git worktree add"
 alias gwtls="git worktree list"
 alias gwtmv="git worktree move"
 alias gwtrm="git worktree remove"
+
+# Git diff
+alias gd="git diff"
+alias gds="git diff --staged"
+
+# Git fetch
+alias gf="git fetch"
+alias gfa="git fetch --all --tags --prune --jobs=10"
