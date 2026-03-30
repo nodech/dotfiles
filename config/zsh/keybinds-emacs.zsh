ac399166cb26ab51253a74fa925396877f4f41e9
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-03-30 22:57:17 +0400

    feat(zsh/keybinds): Deprecate mixed inputs, just emacs.

--- config/zsh/keybinds-emacs.zsh
+++ config/zsh/keybinds-emacs.zsh
@@ -8,3 +8,17 @@ bindkey '^X^E' edit-command-line
 bindkey '^U' backward-kill-line
 bindkey '^K' kill-line
 bindkey '^[k' kill-whole-line
+
+# autoload -U history-search-end
+# zle -N history-beginning-search-backward-end history-search-end
+# zle -N history-beginning-search-forward-end history-search-end
+#
+# bindkey '^K' history-beginning-search-backward-end
+# bindkey '^J' history-beginning-search-forward-end
+
+autoload -U up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey '^P' up-line-or-beginning-search
+bindkey '^N' down-line-or-beginning-search
