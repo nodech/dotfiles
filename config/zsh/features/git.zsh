3a36ed73b0a1a13aa9d34fa3d884c6a151df706f
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-03-30 18:01:37 +0400

    feat(zsh/git): Add more aliases.

--- config/zsh/features/git.zsh
+++ config/zsh/features/git.zsh
@@ -5,7 +5,16 @@ gfwd() {
 }
 
 alias gback="git checkout HEAD~"
+
+# Common
 alias gst="git status"
 alias gr="git remote"
 alias gco="git checkout"
 alias gc="git commit --verbose"
+
+# Worktree
+alias gwt="git worktree"
+alias gwta="git worktree add"
+alias gwtls="git worktree list"
+alias gwtmv="git worktree move"
+alias gwtrm="git worktree remove"
