4673775883e28cf116e385ec05dd973fc25a83dc
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-04-14 15:14:56 +0400

    feat(waybar): Add sms to the tray count.

--- config/waybar/scripts/tray-count.sh
+++ config/waybar/scripts/tray-count.sh
@@ -33,23 +33,41 @@ local watchexec_count
 watchexec_count=$(pgrep -c watchexec 2>/dev/null)
 watchexec_count=${watchexec_count:-0}
 
-[[ $nvim_count -eq 0 && $docker_count -eq 0 && $watchexec_count -eq 0 ]] && exit 0
+local sms_count
+sms_count=$(sms list --no-header --unread 2>/dev/null | wc -l)
+sms_count=${sms_count:-0}
+
+typeset -A counts
+typeset -A icons
+typeset -A colors
+
+# collect counts...
+counts[nvim]=$nvim_count
+counts[docker]=$docker_count
+counts[watchexec]=$watchexec_count
+counts[sms]=$sms_count
+
+icons[nvim]=''
+icons[docker]=''
+icons[watchexec]='󰦖'
+icons[sms]='󰍦'
+
+colors[nvim]='#39FF14'
+colors[docker]='#2496ED'
+colors[watchexec]='#FF8C00'
+colors[sms]='#34C759'
 
 local text=""
 
-if [[ $nvim_count -gt 0 ]]; then
-  text+="<span color='#39FF14'></span><sub><b>$nvim_count</b></sub>"
-fi
+for name in nvim docker watchexec sms; do
+  local count=${counts[$name]:-0}
+  (( count > 0 )) || continue
 
-if [[ $docker_count -gt 0 ]]; then
   [[ -n "$text" ]] && text+=" "
-  text+="<span color='#2496ED'></span><sub><b>$docker_count</b></sub>"
-fi
+  text+="<span color='${colors[$name]}'>${icons[$name]}</span><sub><b>${count}</b></sub>"
+done
 
-if [[ $watchexec_count -gt 0 ]]; then
-  [[ -n "$text" ]] && text+=" "
-  text+="<span color='#FF8C00'>󰦖</span><sub><b>$watchexec_count</b></sub>"
-fi
+[[ -z "$text" ]] && exit 0
 
 jq -cn \
   --arg text "$text" \
