bc51bf3f83a611ee5b228e04d5d9b9d3a7598be9
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-04-14 19:38:55 +0400

    fix(sway/onmonitor): Exit if we can't connect to sway ipc sock.

--- config/sway/scripts/sway-onmonitor.sh
+++ config/sway/scripts/sway-onmonitor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-    swaymsg -t subscribe '["output"]'
+    swaymsg -t subscribe '["output"]' || exit 1
+
     $XDG_CONFIG_HOME/sway/scripts/sway-monabove.sh
 done
