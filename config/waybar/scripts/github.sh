d9e6195193f6e50bcb874c941e834d7899728c87
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-04-14 18:32:38 +0400

    fix(waybar/github): fix typo.

--- config/waybar/scripts/github.sh
+++ config/waybar/scripts/github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-count=`gh api notifications | jq '. | length'
+count=$(gh api notifications | jq '. | length')
 
 if [[ "$count" != "0" ]]; then
     echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
