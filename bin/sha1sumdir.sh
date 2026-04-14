aee91cd042cb7a1ee409aa5f41120844bd711235
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-04-14 15:35:14 +0400

    fix(bin): sha1sumdir.

--- bin/sha1sumdir.sh
+++ bin/sha1sumdir.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-[[ ! -d $1 ]] && echo "Could not find directory: \"$1\"" && exit 1
-
-# Started from: https://stackoverflow.com/a/545413/3607360
-files=`find $1 -type f | sort | grep -v \/.git\/`
-
-if [[ -f ~/.gitignore_global ]]; then
-  #echo "Filtering with global gitignore"
-  files=`echo "$files" | grep -vFf ~/.gitignore_global`
+if [[ -z "$1" || ! -d "$1" ]]; then
+  printf 'error: directory not found: "%s"\n' "$1" >&2
+  exit 1
 fi
 
-# Only works if the directory itself has .gitignore
-# It does not do it recursively. good enough for now
-if [[ -f $1/.gitignore ]]; then
-  #echo "Filtering with $1/.gitignore"
+allHashes=$(
+  cd "$1" &&
+  LC_ALL=C fd -H -t f -0 \
+    | xargs -0 sha1sum \
+    | sort 
+)
 
-  # removes empty lines
-  gitignore=`cat $1/.gitignore | sed '/^$/d'`
-  files=`echo "$files" | grep -vFf <(echo "$gitignore")`
-fi
 
-allHashes=`echo "$files" | xargs sha1sum`
-
-finalHash=`echo $allHashes | sha1sum`
-echo "$finalHash \"$1\""
+printf '%s\n' "$allHashes" | sha1sum
